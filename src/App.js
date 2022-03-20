428357033a0eb8845fa73c84a0e4dc9fd2fc8bc5
Kazu Takahashi <num96kazu19@icloud.com>
2022-03-20 22:45:05 +0900

    大枠の作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">TODO APP </div>;
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(todo);
+    setTodo("");
+  };
+
+  return (
+    <div className="App">
+      <h3>TODO APP</h3>
+      <form action="" type="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="やる事を入力"
+          value={todo}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setTodo(e.target.value);
+          }}
+        />
+        <button className="btn">追加</button>
+      </form>
+
+      <div className="todo-item">
+        <ul className="undone-todo">
+          <p>未完了</p>
+          <li>1</li>
+        </ul>
+
+        <ul className="done-todo">
+          <p>完了</p>
+          <li>1</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
